539f634d6a04e2c251f64be9004b8427dc338c8d
Daniel Berndt <daniel.berndt@gmail.com>
2022-07-15 21:50:36 +0200

    setup minimal webpack example

--- frontend/src/App.css.js
+++ frontend/src/App.css.js
@@ -1,6 +1,7 @@
 import {dsStyles} from "@mono/ds/dsStyles.css";
 import {style} from "@vanilla-extract/css";
 
+// crashes when saving
 export const myStyle = style([
   dsStyles.display.flex,
   {
---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {VanillaExtractPlugin} = require("@vanilla-extract/webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src", "index.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ["react-refresh/babel"],
+            presets: ["@babel/preset-env", ["@babel/preset-react", {runtime: "automatic"}]],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+    new ReactRefreshWebpackPlugin(),
+    new VanillaExtractPlugin({externals: "@mono/ds/*"}),
+  ],
+};
